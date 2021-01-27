1e1a0680fc9d9eb8f133962a9facb7d430a90a3b
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-27 20:28:47 +0200

    cleanup, started adding routing

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import { React, Component } from 'react';
 import Post from './Post/Post';
 import axios from 'axios';
+import './Posts.css';
+
 
 class Posts extends Component {
     state = {
--- src/containers/Main/MainPage.js
+++ src/containers/Main/MainPage.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-// import Logo from '../../components/Logo/Logo'
+import { Route } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout';
 import Posts from '../../components/Posts/Posts';
 
-import './MainPage.css';
+// import './MainPage.css';
 
 class MainPage extends Component {
-    
     render() {
         return (
             <div className="MainPage">
-                <Layout>
-                    <Posts />
-                </Layout>
+                <Layout/>
+                <Route path="/" exact component={Posts}/>
             </div>
         )
     }
