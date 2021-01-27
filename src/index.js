39190216a27a2071ad574ab3d425fd87ed56a69d
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-27 09:39:27 +0200

    added axios interceptors, edited html

--- src/index.js
+++ src/index.js
@@ -5,13 +5,30 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.headers.common['Authorization'] = 'SOME_TOKEN'
 
+axios.interceptors.request.use(req => {
+	console.log(req);
+  	return req;
+}, error => {
+	  console.log(error);
+	  return Promise.reject(error);
+})
+
+axios.interceptors.response.use(req => {
+	console.log(req);
+	return req;
+}, error => {
+	console.log(error);
+	return Promise.reject(error);
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  	<React.StrictMode>
+    	<App />
+  	</React.StrictMode>,
+  	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
