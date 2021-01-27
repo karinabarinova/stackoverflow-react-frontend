d023e3e11e3bfa5c5fe4af8bc8d35b45f29e4bdc
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-27 21:56:35 +0200

    removed logos, added working fullPost display

--- src/components/Posts/FullPost/FullPost.js
+++ src/components/Posts/FullPost/FullPost.js
@@ -6,11 +6,12 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
-    componentDidUpdate () {
+    componentDidMount () {
+        console.log(this.props)
         console.log("Full Post")
-        if (this.props.id)
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+        if (this.props.match.params.id)
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(res => {
                         console.log(res)
                         this.setState({loadedPost: res.data})
--- src/containers/Main/MainPage.js
+++ src/containers/Main/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout';
 import Posts from '../../components/Posts/Posts';
+import FullPost from '../../components/Posts/FullPost/FullPost';
 
 import './MainPage.css';
 
@@ -12,7 +13,7 @@ class MainPage extends Component {
                 <Layout/>
                 <Route path="/" exact component={Posts}/>
                 {/* <Route path="/new-post" component={NewPost} /> */}
-                <Route path="/:id" exact component={Posts} />
+                <Route path="/posts/:id" exact component={FullPost} />
             </div>
         )
     }
