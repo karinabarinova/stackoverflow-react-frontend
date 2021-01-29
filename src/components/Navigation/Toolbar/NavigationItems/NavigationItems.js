3cf261ebfba2d0c1e36d81273bc52f9e49f788b0
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-29 19:19:03 +0200

    added Dashboard cllass

--- src/components/Navigation/Toolbar/NavigationItems/NavigationItems.js
+++ src/components/Navigation/Toolbar/NavigationItems/NavigationItems.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 import Search from '../Search/Search';
@@ -18,7 +20,7 @@ const navigationItems = (props) => (
             ? <><NavigationItem link="/login">Sign In</NavigationItem>
             <NavigationItem link="/register">Sign Up</NavigationItem></>
             : <NavigationItem link="/logout">Log out</NavigationItem>}
-        {props.isAuthenticated ? <div><p>{props.loggedInUser}</p></div> : null}
+        {props.isAuthenticated ? <div><NavLink className={classes.Dashboard} to="/">{props.loggedInUser}</NavLink></div> : null}
     </ul>
 )
 
