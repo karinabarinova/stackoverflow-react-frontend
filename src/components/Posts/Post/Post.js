61e88f139cf965f36c2391ad6f90a0a225f56c74
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-01 12:56:02 +0200

    working with flexbox in Posts

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -25,41 +25,50 @@ class Post extends Component {
         const changedDate = `${date.getUTCDate()}.${date.getUTCMonth() + 1}.${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
         return (
             <article className={classes.Post} onClick={this.props.clicked}>
-                <div className={classes.Postblock2}>
-                    <div>
-                        <div className={classes.arrowUp}></div>
-                        <span>{this.props.rating}</span>
-                        <div className={classes.arrowDown}></div>
+                <div className={classes.column}>
+                    {/* <div className={classes.Postblock2}>
+                        <div>
+                            <div className={classes.arrowUp}></div>
+                            <span>{this.props.rating}</span>
+                            <div className={classes.arrowDown}></div>
+                        </div>
+                    </div> */}
+                    <div className={classes.Postblock1}>
+                        <div>
+                            <span>{this.props.rating}</span>
+                        </div>
+                        <div>Votes</div>
+                    </div>
+                    <div className={classes.Postblock1}>
+                        <div>
+                            <span>{this.state.commentsCount}</span>
+                        </div>
+                        <div>Answers</div>
                     </div>
                 </div>
-                <div className={classes.Postblock1}>
-                    <div>
-                        <span>{this.props.rating}</span>
-                    </div>
-                    <div>Votes</div>
+                <div className={classes.column2}>
+                        <div className={classes.title}>
+                            <h1>{this.props.title}</h1>
+                        </div>
+                        <div className={classes.content}>
+                            <ShowMoreText
+                            lines={3}
+                            more='Show more'
+                            less='Show less'
+                            className={classes.contentCSS}
+                            anchorClass='my-anchor-css-class'
+                            onClick={this.executeOnClick}
+                            expanded={false}
+                            width={0}>
+                                <div>{this.props.content}</div>
+                            </ShowMoreText>
+                        </div>
+                        <div className={classes.Info}>
+                            <div>asked {changedDate}</div>
+                            <div className={classes.Author}>{this.props.author}</div>
+                        </div>                    
                 </div>
-                <div className={classes.Postblock1}>
-                    <div>
-                        <span>{this.state.commentsCount}</span>
-                    </div>
-                    <div>Answers</div>
-                </div>
-                <h1>{this.props.title}</h1>
-                <ShowMoreText
-                lines={3}
-                more='Show more'
-                less='Show less'
-                className={classes.contentCSS}
-                anchorClass='my-anchor-css-class'
-                onClick={this.executeOnClick}
-                expanded={false}
-                width={280}>
-                    <div>{this.props.content}</div>
-                </ShowMoreText>
-                <div className={classes.Info}>
-                    <div>asked {changedDate}</div>
-                    <div className={classes.Author}>{this.props.author}</div>
-                </div>
+                
             </article>
         )
     }
