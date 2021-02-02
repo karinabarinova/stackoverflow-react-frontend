241cbfc0d38bf0ce8d2f8ae2585b0b3ccec7d1f1
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-02 12:18:45 +0200

    added list of user posted questions and answers

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,34 +2,86 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import classes from './Dashboard.module.css';
 import Button from '../Button/Button';
+import Post from '../Posts/Post/Post';
 import defaultUserAvatar from '../../assets/images/default-avatar.png';
+import Comment from '../Comment/Comment';
 
 class Dashboard extends Component {
     state = {
         questions: [],
         answers: [],
-        user: {}
+        user: {},
+        error: false
     }
 
     componentDidMount() {
         const id = +localStorage.getItem('userId');
         axios.get('/users/' + id)
             .then(res => {
-                // console.log(res.data)
                 this.setState({ user: res.data })
             })
             .catch(e => {
-                console.log(e)
+                this.setState({error: true})
+                console.log("Dashboard")
+            })
+        axios.get('/users/' + id + '/posts', {headers: {
+            'authorization': `Basic ${localStorage.getItem('token')}`
+        }})
+            .then(res => {
+                this.setState({ questions: res.data })
+            })
+            .catch(e => {
+                this.setState({error: true})
+                console.log("Dashboard")
+            })
+        axios.get('/users/' + id + '/comments', {headers: {
+            'authorization': `Basic ${localStorage.getItem('token')}`
+        }})
+            .then(res => {
+                this.setState({ answers: res.data })
+            })
+            .catch(e => {
+                this.setState({error: true})
                 console.log("Dashboard")
             })
     }
 
+    postSelectedHandler = (id) => {
+        this.props.history.push('/posts/' + id);
+    }
+
     render() {
+        let questions = <p>You have not asked any questions yet ;(</p>;
+        let answers = <p>You have not answer to any questions yet ;(</p>;
+
+        if (!this.state.error) {
+            questions = this.state.questions.map(post => {
+                return <Post 
+                        key={post.id}
+                        id={post.id}
+                        title={post.title}
+                        authorId={post.author}
+                        content={post.content}
+                        author={post.author}
+                        rating={post.rating}
+                        publish_date={post.publish_date}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+            })
+            answers = this.state.answers.map(answer => {
+                return <Comment 
+                            key={answer.id}
+                            content={answer.content}
+                            author={answer.author} //TODO: incorrect author
+                            publish_date={answer.publish_date}
+                        />
+            })
+        }
         return(
             <div className={classes.Dashboard}>
                 <div className={classes.Info}>
                     <div>
-                        <img src={this.state.user.avatar ? "localhost:3001/api" + localStorage.getItem('avatar').replace('resources', '') : defaultUserAvatar} alt="Profile {hoto" />
+                        <img src={localStorage.getItem('avatar') !== 'undefined' ? "localhost:3001/api/" + localStorage.getItem('avatar').replace('resources', '') : defaultUserAvatar} alt="Profile {hoto" />
                     </div>
                     <div className={classes.generalInfo}>
                         <p><b>User:</b> {this.state.user.login}</p>
@@ -38,13 +90,17 @@ class Dashboard extends Component {
                         <p><b>Full name:</b> {this.state.user.fullName}</p>
                         <p><b>Your rating:</b> {this.state.user.rating}</p>
                     </div>
+                    <Button btnType="DashboardSuccess">Upload Avatar</Button>
+                    <Button btnType="DashboardDanger">Change Password</Button>
                 </div>
-                <Button btnType="DashboardSuccess">Upload Avatar</Button>
-                <Button btnType="DashboardDanger">Change Password</Button>
-                <div className={classes.questions}>
-
+                <div className={classes.Content}>
+                    <div className={classes.questions}>
+                        {questions}
+                    </div>
+                    <div className={classes.answers}>
+                        {answers}
+                    </div>
                 </div>
-                <div className={classes.answers}></div>
             </div>
         ) 
     }
