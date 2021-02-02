2a6bad79e6faba956c2894fef54f06f74485f7f8
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-02 11:31:54 +0200

    edited dashboard styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,26 +2,49 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import classes from './Dashboard.module.css';
 import Button from '../Button/Button';
+import defaultUserAvatar from '../../assets/images/default-avatar.png';
 
 class Dashboard extends Component {
     state = {
         questions: [],
         answers: [],
-        login: null,
-        rating: null,
-        name: null,
-        email: null,
-        avatar: null
+        user: {}
     }
 
-    // componentDidMount() {
-    //     axios.get()
-    // }
+    componentDidMount() {
+        const id = +localStorage.getItem('userId');
+        axios.get('/users/' + id)
+            .then(res => {
+                // console.log(res.data)
+                this.setState({ user: res.data })
+            })
+            .catch(e => {
+                console.log(e)
+                console.log("Dashboard")
+            })
+    }
+
     render() {
         return(
             <div className={classes.Dashboard}>
-                <Button btnType="Success">Upload Avatar</Button>
-                <Button btnType="Danger">Change Password</Button>
+                <div className={classes.Info}>
+                    <div>
+                        <img src={this.state.user.avatar ? "localhost:3001/api" + localStorage.getItem('avatar').replace('resources', '') : defaultUserAvatar} alt="Profile {hoto" />
+                    </div>
+                    <div className={classes.generalInfo}>
+                        <p><b>User:</b> {this.state.user.login}</p>
+                        <p><b>Account created at:</b> {this.state.user.created}</p>
+                        <p><b>Your email:</b> {this.state.user.email}</p>
+                        <p><b>Full name:</b> {this.state.user.fullName}</p>
+                        <p><b>Your rating:</b> {this.state.user.rating}</p>
+                    </div>
+                </div>
+                <Button btnType="DashboardSuccess">Upload Avatar</Button>
+                <Button btnType="DashboardDanger">Change Password</Button>
+                <div className={classes.questions}>
+
+                </div>
+                <div className={classes.answers}></div>
             </div>
         ) 
     }
