681d12325d05ceebc5024945c2484de8b1d93f4a
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-02 18:54:26 +0200

    added axios request to create new posts

--- src/components/Posts/NewPost/NewPost.js
+++ src/components/Posts/NewPost/NewPost.js
@@ -7,16 +7,18 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: ''
+        categories: ''
     }
 
     postDataHandler = () => {
         const post = {
             title: this.state.title,
             content: this.state.content,
-            author: this.state.author
+            categories: this.state.categories
         }
-        axios.post('/posts', post)
+        axios.post('/posts', post, {headers: {
+            'authorization': `Basic ${localStorage.getItem('token')}`
+        }})
             .then((res) => {
                 console.log(res);
             })
@@ -34,11 +36,12 @@ class NewPost extends Component {
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 <label>Content</label>
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <label>Categories</label>
+                <input type="text" value={this.state.categories} onChange={(event) => this.setState({categories: event.target.value})} />
+                {/* <select value={this.state.categories} onChange={(event) => this.setState({categories: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
-                </select>
+                </select> */}
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         )
