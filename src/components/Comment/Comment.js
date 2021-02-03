609566e9a6b1e07793bedc9729bf3a5e87e1c1cc
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-03 19:38:34 +0200

    added flexbox to Comment component

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Comment.css';
+import defaultUserAvatar from '../../assets/images/default-avatar.png';
 
-const comment = (props) => (
-    <div className="Comment">
-        <p>{props.content}</p>
-        <p>answered {props.publish_date.replace('.000Z', '').replace('T', ' ')}</p>
-        <p>{props.author}</p>
-    </div>
-);
+class Comment extends Component {
+    state = {};
 
-export default comment;
+    render() {
+        return (
+            <div className="Comment">
+                <div className="ratingInfo">
+                    <div onClick={() => this.postLikeHandler()} className="arrowUp"></div>
+                    <span>0</span>
+                    <div onClick={() => this.postDislikeHandler()} className="arrowDown"></div>
+                </div>
+                <div className="content">
+                    <p>{this.props.content}</p>
+                </div>
+                <div className="aboutAuthor">
+                    <div className="Info">
+                        <div>answered {this.props.publish_date.replace('.000Z', '').replace('T', ' ')}</div>
+                        <div className="avatar"><img src={ this.state.author ?  "http://localhost:3001/" + this.state.author.avatar.replace('resources', '') : defaultUserAvatar} target="_blank" alt="author avatar" /></div>
+                        <div className="Author">{this.state.author}</div>
+                    </div> 
+                </div>   
+                {/* <p>{this.props.author}</p> */}
+            </div>
+        );
+    }
+} 
+
+export default Comment;
